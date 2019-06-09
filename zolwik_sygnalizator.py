affc54936d855b9d9736ff25b851c8b2e232b89c
sebwas23 <51231258+sebwas23@users.noreply.github.com>
2019-06-09 10:37:42 +0200

    Update zolwik_sygnalizator.py

--- zolwik_sygnalizator.py
+++ zolwik_sygnalizator.py
@@ -1,1 +1,14 @@
 import turtle
+t = turtle.Turtle(shape="turtle")
+def kolko(kolor1,kolor2,x,y,r):
+    t.color(kolor1,kolor2)
+    t.penup()
+    t.setpos(x,y)
+    t.pendown()
+    t.begin_fill()
+    t.circle(r)
+    t.end_fill()
+
+kolko("red","red",0,100,50)
+kolko("yellow","yellow",0,-50,50)
+kolko("green","green",0,-200,50)
