0493ab216d0107d790c189f493c11f75f1830e69
sebwas23 <51231258+sebwas23@users.noreply.github.com>
2019-06-10 20:47:55 +0200

    Create szyfr_cezara_odkoduj.py

---
+++ szyfr_cezara_odkoduj.py
@@ -0,0 +1,4 @@
+a = "Pqvtbgdc rculk k bccpicżqycpkc, żgda pcrtcyfę fqiłędpkg eqś btqbwokgć, rtbgżwć, c pkg vanmq ubadmq rtbgłmpąć. Wkęmubqść nwfbk pkg rqśykęec pc vq ebcuw"
+
+dekod = a.maketrans("cdefghijklmnopqrstuvwxyzab", "abcdefghijklmnopqrstuvwxyz")
+print (a.translate(dekod))
